e3211e381c856b64df2f364acd20b3416073aa59
rafifathur <rafifathur.rahman20@gmai.com>
2024-06-04 16:56:16 +0700

    Fix : fixing wrong condition dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -13,11 +13,11 @@ class DashboardController extends Controller
      */
     public function dashboard()
     {
-        $dashboard['total_income'] = SalesOrder::whereMonth('created_by', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'))->sum('grand_sell_price');
-        $dashboard['total_profit'] = SalesOrder::whereMonth('created_by', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'))->sum('grand_profit_price');
-        $dashboard['total_sales_order'] = count(SalesOrder::whereMonth('created_by', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'))->get());
+        $dashboard['total_income'] = SalesOrder::whereMonth('created_at', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'))->sum('grand_sell_price');
+        $dashboard['total_profit'] = SalesOrder::whereMonth('created_at', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'))->sum('grand_profit_price');
+        $dashboard['total_sales_order'] = count(SalesOrder::whereMonth('created_at', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'))->get());
         $dashboard['total_product_sold'] = SalesOrderItem::whereHas('salesOrder', function ($query) {
-            return $query->whereMonth('created_by', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'));
+            return $query->whereMonth('created_at', date('m'))->where('deleted_at', null)->whereYear('created_at', date('Y'));
         })->sum('qty');
         return view('dashboard', compact('dashboard'));
     }
