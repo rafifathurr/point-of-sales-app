5d86e065dd964f6b28e2aba85c3974864157610c
rafifathur <rafifathur.rahman20@gmai.com>
2024-07-24 07:02:43 +0700

    Fix : fixing with adding comment

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -9,34 +9,61 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    /**
+     * Construct Middleware Access
+     */
     public function __construct()
     {
+        /**
+         * As Guest For All Function Controller Except Logout Function (Auth Middleware)
+         */
         $this->middleware('guest')->except('logout');
     }
 
+    /**
+     * View Login Function
+     */
     public function login()
     {
         return view('auth.login');
     }
 
+    /**
+     * Authenticate Login Function
+     */
     public function authenticate(Request $request)
     {
         try {
+            /**
+             * Filtering Email or Username Value
+             */
             $email_or_username = $request->input('username');
             $field = filter_var($email_or_username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
             $request->merge([$field => $email_or_username]);
 
+            /**
+             * Request Validation
+             */
             $request->validate([
                 $field => 'required',
                 'password' => 'required',
             ]);
 
+            /**
+             * User Validation Fom Request Email or Username
+             */
             $user = User::where($field, $email_or_username)->first();
 
+            /**
+             * Validation User Check
+             */
             if (!is_null($user)) {
+
+                /**
+                 * Login Authenticate
+                 */
                 if (Auth::attempt([$field => $email_or_username, 'password' => $request->password], isset($request->remember))) {
                     $request->session()->regenerate();
-                    // For Request Url
                     $intended_url = session()->pull('url.intended', route('home'));
                     return redirect()->to($intended_url);
                 } else {
@@ -56,6 +83,9 @@ class AuthController extends Controller
         }
     }
 
+    /**
+     * Logout Function
+     */
     public function logout(Request $request)
     {
         Auth::logout();
